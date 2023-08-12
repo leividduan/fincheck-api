69213cc078e44419e89d9b125fb626c28b0b6222
Deivid <deivid.cardos@gmail.com>
2023-08-12 00:49:35 -0300

    day 5

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { AuthGuard } from '../auth/auth.guard';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, AuthGuard],
 })
 export class UsersModule {}
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,57 +1,18 @@
-import { ConflictException, Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { hash } from 'bcryptjs';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { UsersRepository } from 'src/shared/database/repositories/users.repositories';
 
 @Injectable()
 export class UsersService {
-  constructor(private readonly usersRepository: UsersRepository) {}
-
-  async create(createUserDto: CreateUserDto) {
-    const { name, email, password } = createUserDto;
-
-    const emailTaken = await this.usersRepository.findUnique({
-      where: { email },
-      select: { id: true },
+  constructor(private readonly usersRepo: UsersRepository) {}
+  getUserById(id) {
+    const user = this.usersRepo.findUnique({
+      where: { id },
     });
 
-    if (emailTaken) {
-      throw new ConflictException('This e-mail is already in use.');
+    if (!user) {
+      throw new NotFoundException('User not found');
     }
 
-    const hashedPassword = await hash(password, 12);
-
-    const user = await this.usersRepository.create({
-      data: {
-        name: name,
-        email: email,
-        password: hashedPassword,
-        categories: {
-          createMany: {
-            data: [
-              // Income
-              { name: 'Salário', icon: 'salary', type: 'INCOME' },
-              { name: 'Freelance', icon: 'freelance', type: 'INCOME' },
-              { name: 'Outro', icon: 'other', type: 'INCOME' },
-              // Expense
-              { name: 'Casa', icon: 'home', type: 'EXPENSE' },
-              { name: 'Alimentação', icon: 'food', type: 'EXPENSE' },
-              { name: 'Educação', icon: 'education', type: 'EXPENSE' },
-              { name: 'Lazer', icon: 'fun', type: 'EXPENSE' },
-              { name: 'Mercado', icon: 'grocery', type: 'EXPENSE' },
-              { name: 'Roupas', icon: 'clothes', type: 'EXPENSE' },
-              { name: 'Transporte', icon: 'transport', type: 'EXPENSE' },
-              { name: 'Viagem', icon: 'travel', type: 'EXPENSE' },
-              { name: 'Outro', icon: 'other', type: 'EXPENSE' },
-            ],
-          },
-        },
-      },
-    });
-
-    return {
-      name: user.name,
-      email: user.email,
-    };
+    return user;
   }
 }
